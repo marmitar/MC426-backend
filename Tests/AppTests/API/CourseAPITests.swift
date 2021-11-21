9891c837fa4d32e2cfa3b781e16559fa124b35f6
Leonardo Rodrigues <leonardosrodrigues1@gmail.com>
2021-11-21 14:48:45 -0300

    Move api routes to test classes static properties

--- Tests/AppTests/API/CourseAPITests.swift
+++ Tests/AppTests/API/CourseAPITests.swift
@@ -2,15 +2,17 @@
 import XCTVapor
 
 final class CourseAPITests: XCTestCase {
+
+    private static let route = "api/curso/"
+
     func testFetchCourseWithVariantsAndValidCodeAndNoVariantSelected() throws {
         let app = Application(.testing)
         defer { app.shutdown() }
         try configure(app)
 
-        let route = "api/curso/"
-        let course = "34/"
+        let url = Self.route + "34/"
 
-        try app.test(.GET, route + course, afterResponse: { res in
+        try app.test(.GET, url, afterResponse: { res in
             XCTAssertEqual(res.status, .ok)
             XCTAssertEqual(res.body.string, "{\"name\":\"Engenharia de Computação - Integral\",\"variant\":[\"AA - Sistemas de Computação\",\"AB - Sistemas e Processos Industriais\",\"AX - Para Matrícula Antes da Opção\"],\"code\":\"34\"}")
         })
@@ -21,11 +23,9 @@ final class CourseAPITests: XCTestCase {
         defer { app.shutdown() }
         try configure(app)
 
-        let route = "api/curso/"
-        let course = "34/"
-        let variant = "2/"
+        let url = Self.route + "34/2/"
 
-        try app.test(.GET, route + course + variant, afterResponse: { res in
+        try app.test(.GET, url, afterResponse: { res in
             XCTAssertEqual(res.status, .ok)
             XCTAssertEqual(res.body.string, "[[\"F 128\",\"F 129\",\"HZ291\",\"MA111\",\"MA141\",\"MC102\",\"QG111\",\"QG122\"],[\"F 228\",\"F 229\",\"LA122\",\"MA211\",\"MA327\",\"MC202\"],[\"EA513\",\"F 315\",\"F 328\",\"F 329\",\"MA311\",\"MC322\"],[\"EA614\",\"EA772\",\"F 428\",\"F 429\",\"MC358\",\"MC404\"],[\"EA773\",\"EA871\",\"EA876\",\"EE400\",\"EE532\",\"MC458\",\"ME323\"],[\"EA202\",\"EA872\",\"EA960\",\"EE534\",\"EM423\",\"MS211\"],[\"EA074\",\"EA201\",\"EA616\",\"EA619\",\"EA979\"],[\"BE310\",\"EA072\",\"EA080\",\"EA721\",\"EA722\",\"EG950\"],[\"CE304\",\"CE838\",\"EA006\",\"EA044\",\"EE610\"],[\"CE738\"]]")
         })
@@ -36,11 +36,9 @@ final class CourseAPITests: XCTestCase {
         defer { app.shutdown() }
         try configure(app)
 
-        let route = "api/curso/"
-        let course = "34/"
-        let variant = "3/"
+        let url = Self.route + "34/3/"
 
-        try app.test(.GET, route + course + variant, afterResponse: { res in
+        try app.test(.GET, url, afterResponse: { res in
             XCTAssertEqual(res.status, .notFound)
         })
     }
@@ -51,10 +49,9 @@ final class CourseAPITests: XCTestCase {
         defer { app.shutdown() }
         try configure(app)
 
-        let route = "api/curso/"
-        let course = "0/"
+        let url = Self.route + "0/"
 
-        try app.test(.GET, route + course, afterResponse: { res in
+        try app.test(.GET, url, afterResponse: { res in
             XCTAssertEqual(res.status, .notFound)
         })
     }
@@ -64,11 +61,9 @@ final class CourseAPITests: XCTestCase {
         defer { app.shutdown() }
         try configure(app)
 
-        let route = "api/curso/"
-        let course = "0/"
-        let variant = "7/"
+        let url = Self.route + "0/7/"
 
-        try app.test(.GET, route + course + variant, afterResponse: { res in
+        try app.test(.GET, url, afterResponse: { res in
             XCTAssertEqual(res.status, .notFound)
         })
     }
@@ -78,11 +73,9 @@ final class CourseAPITests: XCTestCase {
         defer { app.shutdown() }
         try configure(app)
 
-        let route = "api/curso/"
-        let course = "11/"
-        let variant = ""
+        let url = Self.route + "11/"
 
-        try app.test(.GET, route + course + variant, afterResponse: { res in
+        try app.test(.GET, url, afterResponse: { res in
             XCTAssertEqual(res.status, .ok)
             XCTAssertEqual(res.body.string, "{\"name\":\"Engenharia Elétrica - Integral\",\"code\":\"11\"}")
         })
@@ -93,11 +86,9 @@ final class CourseAPITests: XCTestCase {
         defer { app.shutdown() }
         try configure(app)
 
-        let route = "api/curso/"
-        let course = "11/"
-        let variant = "0"
+        let url = Self.route + "11/0/"
 
-        try app.test(.GET, route + course + variant, afterResponse: { res in
+        try app.test(.GET, url, afterResponse: { res in
             XCTAssertEqual(res.status, .ok)
             XCTAssertEqual(res.body.string, "[[\"EA772\",\"EM230\",\"EM312\",\"F 128\",\"F 129\",\"MA111\",\"MA141\"],[\"EA513\",\"EA773\",\"F 228\",\"F 229\",\"MA211\",\"MA327\",\"MC102\"],[\"EA611\",\"EA869\",\"EE103\",\"EM524\",\"F 315\",\"MA311\",\"ME323\"],[\"EA871\",\"EE300\",\"EE301\",\"EE400\",\"EE521\",\"LA122\",\"MS211\",\"QG111\",\"QG122\"],[\"EA075\",\"EA614\",\"EE410\",\"EE522\",\"EE540\",\"EM423\",\"ET520\",\"ET521\"],[\"EA616\",\"EA619\",\"EA879\",\"EE530\",\"EE754\",\"EE881\",\"ET620\",\"ET621\"],[\"EA076\",\"EA721\",\"EA722\",\"EE531\",\"EE640\",\"EE755\",\"EE882\",\"ET720\",\"ET911\"],[\"CE738\",\"CE838\",\"EA044\",\"EE610\",\"EE641\",\"EE833\"],[\"BE310\",\"CE304\",\"EA006\"],[]]")
         })
@@ -108,11 +99,9 @@ final class CourseAPITests: XCTestCase {
         defer { app.shutdown() }
         try configure(app)
 
-        let route = "api/curso/"
-        let course = "11/"
-        let variant = "1"
+        let url = Self.route + "11/1/"
 
-        try app.test(.GET, route + course + variant, afterResponse: { res in
+        try app.test(.GET, url, afterResponse: { res in
             XCTAssertEqual(res.status, .notFound)
         })
     }
--- Tests/AppTests/API/DisciplineAPITests.swift
+++ Tests/AppTests/API/DisciplineAPITests.swift
@@ -2,15 +2,17 @@
 import XCTVapor
 
 final class DisciplineAPITests: XCTestCase {
+
+    private static let route = "api/disciplina/"
+
     func testFetchDisciplineWithValidCode() throws {
         let app = Application(.testing)
         defer { app.shutdown() }
         try configure(app)
 
-        let route = "api/disciplina/"
-        let discipline1 = "MC102"
+        let url = Self.route + "MC102"
 
-        try app.test(.GET, route + discipline1, afterResponse: { res in
+        try app.test(.GET, url, afterResponse: { res in
             XCTAssertEqual(res.status, .ok)
             XCTAssertEqual(res.body.string, "{\"code\":\"MC102\",\"reqBy\":[\"CV074\",\"CV632\",\"EA044\",\"EA060\",\"EA869\",\"EA954\",\"EG940\",\"EM008\",\"EM024\",\"EQ048\",\"F 790\",\"FA103\",\"FA374\",\"MC202\",\"MC886\",\"MC886\",\"MC949\",\"MC949\",\"ME315\",\"MS211\",\"MS505\",\"MS614\"],\"syllabus\":\"Conceitos básicos de organização de computadores. Construção de algoritmos e sua representação em pseudocódigo e linguagens de alto nível. Desenvolvimento sistemático e implementação de programas. Estruturação, depuração, testes e documentação de programas. Resolução de problemas.\",\"name\":\"Algoritmos e Programação de Computadores\",\"credits\":6}")
         })
@@ -21,10 +23,9 @@ final class DisciplineAPITests: XCTestCase {
         defer { app.shutdown() }
         try configure(app)
 
-        let route = "api/disciplina/"
-        let discipline1 = "MC1022"
+        let url = Self.route + "MC1022"
 
-        try app.test(.GET, route + discipline1, afterResponse: { res in
+        try app.test(.GET, url, afterResponse: { res in
             XCTAssertEqual(res.status, .notFound)
         })
     }
@@ -34,10 +35,9 @@ final class DisciplineAPITests: XCTestCase {
         defer { app.shutdown() }
         try configure(app)
 
-        let route = "api/disciplina/"
-        let discipline1 = "MC10"
+        let url = Self.route + "MC10"
 
-        try app.test(.GET, route + discipline1, afterResponse: { res in
+        try app.test(.GET, url, afterResponse: { res in
             XCTAssertEqual(res.status, .notFound)
         })
     }
@@ -47,10 +47,9 @@ final class DisciplineAPITests: XCTestCase {
         defer { app.shutdown() }
         try configure(app)
 
-        let route = "api/disciplina/"
-        let discipline1 = "AA200"
+        let url = Self.route + "AA200"
 
-        try app.test(.GET, route + discipline1, afterResponse: { res in
+        try app.test(.GET, url, afterResponse: { res in
             XCTAssertEqual(res.status, .notFound)
         })
     }
@@ -60,10 +59,9 @@ final class DisciplineAPITests: XCTestCase {
         defer { app.shutdown() }
         try configure(app)
 
-        let route = "api/disciplina/"
-        let discipline1 = "mc102"
+        let url = Self.route + "mc102"
 
-        try app.test(.GET, route + discipline1, afterResponse: { res in
+        try app.test(.GET, url, afterResponse: { res in
             XCTAssertEqual(res.status, .notFound)
         })
     }
