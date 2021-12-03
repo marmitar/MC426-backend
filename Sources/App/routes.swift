4c6d54bb765a7dcf315dfa3ce37000bc40794938
Tiago de Paula <tiagodepalves@gmail.com>
2021-12-03 04:53:37 -0300

    Respond BADREQUEST when the endpoint is /api but unused

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,13 +1,32 @@
 import Vapor
 
 
-func routes(_ app: Application) throws {
+func routes(_ app: RoutesBuilder) throws {
     // Página principal: carrega o frontend
     app.get("") { req in
         getIndexHTML(for: req)
     }
 
-    let api = app.grouped("api")
+    // endpoint para requisições da API
+    api_routes(app.grouped("api"))
+
+    // Fallback: deixa o frontend tratar
+    app.get("**") { req in
+        getIndexHTML(for: req)
+    }
+}
+
+/// Carrega o HTML gerado pelo frontend.
+private func getIndexHTML(for req: Request) -> Response {
+    req.fileio.streamFile(at: "Public/index.html", mediaType: .html)
+}
+
+/// Rotas usadas pela API.
+private func api_routes(_ api: RoutesBuilder) {
+    // API: resposta padrão
+    api.get("") { req -> Response in
+        throw Abort(.noContent)
+    }
 
     // API: busca textual entre vários elementos.
     api.get("busca") { req in
@@ -29,13 +48,8 @@ func routes(_ app: Application) throws {
         try Course.Controller.shared.fetchCourseTree(req)
     }
 
-    // Fallback: deixa o frontend tratar
-    app.get("**") { req in
-        getIndexHTML(for: req)
+    // API: desconhecida
+    api.get("**") { req -> Response in
+        throw Abort(.badRequest)
     }
-}
-
-/// Carrega o HTML gerado pelo frontend.
-private func getIndexHTML(for req: Request) -> Response {
-    req.fileio.streamFile(at: "Public/index.html", mediaType: .html)
 }
